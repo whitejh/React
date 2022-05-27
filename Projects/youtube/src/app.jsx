585b820819788b9148e521edf7105988cea170de
whitejh <jhk0806@gmail.com>
2022-05-27 15:37:08 +0900

    popular 목록 API 이용해서 받아와서 보여주기

--- Projects/youtube/src/app.jsx
+++ Projects/youtube/src/app.jsx
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import "./app.css";
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0", requestOptions)
+    .then(response => response.json())
+    .then(result => setVideos(result.items))
+    .catch(error => console.log('error', error));
+    }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
