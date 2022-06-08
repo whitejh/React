f53a6a2446c8cff6ddfb6667fd875de29076ba7b
whitejh <jhk0806@gmail.com>
2022-06-08 17:13:36 +0900

    리스트 아이템 만들기

--- Projects/youtube/src/app.jsx
+++ Projects/youtube/src/app.jsx
@@ -3,7 +3,7 @@ import "./app.css";
 import VideoList from './components/video_list/video_list';
 
 function App() {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]); // 비디오 목록
 
   useEffect(() => {
     const requestOptions = {
@@ -15,9 +15,10 @@ function App() {
     .then(response => response.json())
     .then(result => setVideos(result.items))
     .catch(error => console.log('error', error));
-    }, []);
+    }, []); // 빈 배열을 2번째 인자로 전달하면 마운트가 되었을 때만 호출
 
   return <VideoList videos={videos} />;
 }
 
 export default App;
+
