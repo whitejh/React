66c26639dba45033bf7175c0207e486c6b813666
whitejh <jhk0806@gmail.com>
2022-06-12 16:17:03 +0900

    검색 기능 구현

--- Projects/youtube/src/app.jsx
+++ Projects/youtube/src/app.jsx
@@ -1,24 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import "./app.css";
-import VideoList from './components/video_list/video_list';
+import React, { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]); // 비디오 목록
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+      method: "GET",
+      redirect: "follow",
+    };
 
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0", requestOptions)
-    .then(response => response.json())
-    .then(result => setVideos(result.items))
-    .catch(error => console.log('error', error));
-    }, []); // 빈 배열을 2번째 인자로 전달하면 마운트가 되었을 때만 호출
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []); // 빈 배열을 2번째 인자로 전달하면 마운트가 되었을 때만 호출
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
-
