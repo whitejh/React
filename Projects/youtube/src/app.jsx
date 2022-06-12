bcfcf4455e73cd2d3808212004e994f7869616ac
whitejh <jhk0806@gmail.com>
2022-06-12 16:57:15 +0900

    검색 기능 리팩토링

--- Projects/youtube/src/app.jsx
+++ Projects/youtube/src/app.jsx
@@ -3,39 +3,18 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]); // 비디오 목록
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD2Aj5UuP9zQ-YLFdIyTA2EqgJgL-yM5p0",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []); // 빈 배열을 2번째 인자로 전달하면 마운트가 되었을 때만 호출
 
   return (
